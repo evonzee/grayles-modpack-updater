7c0c23ff92e3efc45196b383d37642bf1085ec9e
eric vz <git@base10.org>
2020-03-07 23:44:14 -0600

    copy latest modpack csv into mods folder on run

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 docker build --tag grayles-modpack-update:latest .
+if [ ! -d mods ]; then
+    mkdir mods
+fi
+cp current-modpack.csv mods/
+
 ex=$(docker ps -a | grep grayles-plus-update)
 if [ "$ex" == "" ]; then
     docker run -v $(pwd)/mods:/mods -ti --name grayles-plus-update grayles-modpack-update perl ./load-mods.pl
